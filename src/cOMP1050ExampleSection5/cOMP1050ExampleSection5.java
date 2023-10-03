ba618a6c28c6ed01b4c3a7ba5d29f61616b52b3e
aiellom2atwit <aiellom2@wit.edu>
2023-10-03 13:47:40 -0400

    Added main and Java class files
    Added main and Java class files made classpath project

---
+++ src/cOMP1050ExampleSection5/cOMP1050ExampleSection5.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package cOMP1050ExampleSection5;
+
+/**
+ * 
+ */
+public class cOMP1050ExampleSection5 {
+
+	/**
+	 * 
+	 */
+	public cOMP1050ExampleSection5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello world!");
+	}
+
+}
